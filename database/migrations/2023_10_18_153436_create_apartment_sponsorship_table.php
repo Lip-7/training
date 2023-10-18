439c774a0b884f10d5745f09da0b0051e5ebe726
Massimiliano-Novello <massinov5@gmail.com>
2023-10-18 17:47:52 +0200

    created sponsor and bridge table

---
+++ database/migrations/2023_10_18_153436_create_apartment_sponsorship_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('apartment_sponsorship', function (Blueprint $table) {
+            $table->foreignId('apartment_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('sponsorship_id')->constrained()->cascadeOnDelete();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('expire_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('apartment_sponsorship');
+    }
+};
