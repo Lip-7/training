3173196addc3dbbad80ad0684856b6c5db974c31
Morella92 <morena.piem@outlook.com>
2023-10-19 16:33:08 +0200

    FEAT user+apartment seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {   
+
+        for ($i = 0; $i < 50; $i++){
+            
+            $newUser = new User();
+
+            $newUser->name = $faker->firstname();
+            $newUser->lastname = $faker->lastname();
+            $newUser->email = $faker->email();
+            $newUser->password = 'training';
+            $newUser->birth_date = $faker->dateTimeBetween('-70 years', '-18 years');
+
+            $newUser->save();
+        };
+    }
+}
