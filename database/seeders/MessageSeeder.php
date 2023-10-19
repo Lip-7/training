4336b5665f8630b2773dd9a63bbf14d85b606ec1
sollendy <edlannino@tutanota.com>
2023-10-19 17:57:00 +0200

    feat: seed di messaggi e servizi

--- database/seeders/MessageSeeder.php
+++ database/seeders/MessageSeeder.php
@@ -2,16 +2,32 @@
 
 namespace Database\Seeders;
 
+use App\Models\Message;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class MessageSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
+
     {
-        //
+        $messaggi = config("messaggi");
+        for($i = 0; $i < 130; $i++) {
+            $newMessage = new Message();
+            $newMessage->name = $faker->firstName();
+            $newMessage->lastname = $faker->lastName();
+            $newMessage->email = $faker->email();
+            $newMessage->text = $messaggi[$i];
+            if($i < 100){
+                $newMessage->apartment_id = $i + 1;
+            } else {
+                $newMessage->apartment_id = $faker->numberBetween(1, 100);
+            }
+            $newMessage->save();
+        }
     }
 }
--- database/seeders/ServiceSeeder.php
+++ database/seeders/ServiceSeeder.php
@@ -2,16 +2,39 @@
 
 namespace Database\Seeders;
 
+use App\Models\Apartment;
+use App\Models\Service;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ServiceSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        $servizi = config("servizi");
+        foreach($servizi as $servizio) {
+            $newService = new Service();
+            $newService->name = $servizio;
+            $newService->save();
+        }
+
+        for ($i = 1; $i < 101; $i++){
+            $numServices = rand(1, 8);
+            $usedServices = [];
+            for($j = 0; $j < $numServices; $j++) {
+                $singleService = rand(1, (count($servizi)));
+                if(in_array($singleService, $usedServices)){
+                    $j--;
+                } else {
+                    array_push($usedServices, $singleService);
+                }
+            }
+            $apartment = Apartment::find($i);
+            $apartment->services()->sync($usedServices);
+        }
     }
 }
