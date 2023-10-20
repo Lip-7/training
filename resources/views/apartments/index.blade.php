3d3864240b45955d8444f94f579d15358eefa10e
sollendy <edlannino@tutanota.com>
2023-10-20 16:24:06 +0200

    feat: View appartamenti costruita

--- resources/views/apartments/index.blade.php
+++ resources/views/apartments/index.blade.php
@@ -6,9 +6,9 @@
     <div class="card" style="width: 18rem;">
         <img src="{{$ap->photo}}" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <h5 class="card-title">{{$ap->name}}</h5>
+          {{-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> --}}
+          <a href="#" class="btn btn-primary">Dettagli</a>
         </div>
       </div>
     @endforeach
