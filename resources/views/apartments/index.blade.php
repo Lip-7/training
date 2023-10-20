293234c8b4a81472ea0a0fae5ef95701f23e5655
sollendy <edlannino@tutanota.com>
2023-10-20 16:20:16 +0200

    fix: risolto problema bootstrap. View appartamenti in costruzione.

--- resources/views/apartments/index.blade.php
+++ resources/views/apartments/index.blade.php
@@ -1,8 +1,10 @@
+@extends('layouts.app')
+@section('content')
 <h1>appartamentoni</h1>
-<div class="container d-flex flex-wrap">
+<div class="container d-flex flex-wrap gap-3">
     @foreach ($apartments as $ap)
     <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
+        <img src="{{$ap->photo}}" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -11,3 +13,4 @@
       </div>
     @endforeach
 </div>
+@endsection
