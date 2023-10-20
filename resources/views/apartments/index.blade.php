73adb3233d15413ec65c1bfe754181b43d671469
sollendy <edlannino@tutanota.com>
2023-10-20 16:28:27 +0200

    feat: aggiunto bottone elimina in view

--- resources/views/apartments/index.blade.php
+++ resources/views/apartments/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('content')
-<h1>appartamentoni</h1>
+<h1 class="text-center">I tuoi appartamenti</h1>
 <div class="container d-flex flex-wrap gap-3">
     @foreach ($apartments as $ap)
     <div class="card" style="width: 18rem;">
@@ -9,6 +9,7 @@
           <h5 class="card-title">{{$ap->name}}</h5>
           {{-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> --}}
           <a href="#" class="btn btn-primary">Dettagli</a>
+          <a href="#" class="btn btn-danger">Rimuovi</a>
         </div>
       </div>
     @endforeach
