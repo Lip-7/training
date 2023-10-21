c4047cb76cfb16f46057a252d61af8f0eb8c89be
sollendy <edlannino@tutanota.com>
2023-10-21 15:37:23 +0200

    feat: inserito form destroy + aggiunte rotte bottoni

--- resources/views/apartments/index.blade.php
+++ resources/views/apartments/index.blade.php
@@ -9,8 +9,15 @@
         <div class="card-body">
           <h5 class="card-title">{{$apartment->name}}</h5>
           {{-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> --}}
-          <a href="{{route("apartments.show", $apartment)}}" class="btn btn-primary">Dettagli</a>
-          <a href="#" class="btn btn-danger">Rimuovi</a>
+          <div class="buttons d-flex gap-1">
+            <a href="{{route("apartments.show", $apartment)}}" class="btn btn-primary">Dettagli</a>
+            <a href="{{route("apartments.edit", $apartment)}}" class="btn btn-success">Modifica</a>
+            <form action="{{route('apartments.destroy', $apartment)}}" method="POST">
+                @csrf
+                  @method('DELETE')
+                    <button class="btn btn-danger" type="submit">Rimuovi</button>
+            </form>
+          </div>
         </div>
       </div>
     @endforeach
