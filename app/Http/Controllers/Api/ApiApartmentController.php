79fa3320f5d33e58560db406fc2c51280e27ebc8
sollendy <edlannino@tutanota.com>
2023-10-25 18:20:20 +0200

    feat: inizializzata chiamata api appartamenti

--- app/Http/Controllers/Api/ApiApartmentController.php
+++ app/Http/Controllers/Api/ApiApartmentController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Apartment;
+use App\Models\Service;
+use App\Models\Sponsorship;
+use App\Models\Visit;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
 
 class ApiApartmentController extends Controller
 {
-    public function search(Request $request)
+    public function index(Request $request)
     {
-        
+        $requestData = $request->all();
+
+        $services = Service::all();
+        $sponsorships = Sponsorship::all();
+        $visits = Visit::all();
+        //inserire un dato posizione
+
+        $query = Apartment::query();
     }
 }
