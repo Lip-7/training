a797d7d89703ef850f1c0de593a0c0c2bdb1e3b7
sollendy <edlannino@tutanota.com>
2023-10-25 16:49:10 +0200

    feat: modificata navbar, aggiunti link, da aggiungere loho

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
             <div class="card">
                 <div class="card-header">
                     {{ __('Dashboard') }}
-                    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                    {{-- <nav class="navbar navbar-expand-lg bg-body-tertiary">
                         <div class="container-fluid">
                           <a class="navbar-brand" href="#">Navbar</a>
                           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -42,7 +42,7 @@
                             </form>
                           </div>
                         </div>
-                      </nav>
+                      </nav> --}}
                 </div>
 
                 <div class="card-body">
