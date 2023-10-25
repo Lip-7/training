5b998664fb4cdfa8db71f37318126688a01259ca
Lip-7 <zhighofficial@gmail.com>
2023-10-25 17:55:08 +0200

    minor fixes

--- resources/js/tomseach.js
+++ resources/js/tomseach.js
@@ -1,22 +1,40 @@
-import axios, { Axios } from "axios";
+import axios from "axios";
+
+function generateHints(hints) {
+    hintsList.innerHTML = ''
+    hints.filter((value, key) => {
+        if (key < 5) {
+            const singleHint = document.createElement('li')
+            singleHint.textContent = value.address.freeformAddress;
+            hintsList.appendChild(singleHint);
+        }
+    })
+}
 
 function search() {
     let userSearch = addressInput.value;
-    axios.get('/user', {
-        params: {
-          ID: 12345
-        }
-      })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
-      .finally(function () {
-        // always executed
-      });
+    console.log(userSearch);
+    if (userSearch.length > 3) {
+        console.log('avviamo chiamata axios', userSearch);
+        axios.get('http://127.0.0.1:8000/api/search', {
+            params: {
+              query: userSearch
+            }
+          })
+          .then(function (response) {
+              console.log(response.data.shortAnswer);
+              generateHints(response.data.shortAnswer);
+          })
+          .catch(function (error) {
+            console.log(error);
+          })
+          .finally(function () {
+
+          });
+    }
 }
 
-const addressInput = document.querySelector('userAddressInput');
-addressInput.addEventListener('change', search);
+const hintsList = document.querySelector('.userAddressHints')
+const addressInput = document.querySelector('.userAddressInput');
+addressInput.addEventListener('input', search);
+console.log(addressInput.value);
