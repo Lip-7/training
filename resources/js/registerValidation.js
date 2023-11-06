9b5f7aeb81b9bdf8b6bcf3a9ecfd1daa834fa20e
Massimiliano-Novello <massinov5@gmail.com>
2023-11-06 16:08:56 +0100

    register validation

--- resources/js/registerValidation.js
+++ resources/js/registerValidation.js
@@ -1,15 +1,16 @@
-const password = document.getElementById("password").value;
-const confirmPassword = document.getElementById("password-confirm").value;
-const errorPassword = document.getElementById("password-confirm");
 const formRegistered = document.getElementById("form-register");
 console.log(formRegistered);
 const button = document.getElementById("button-register");
+console.log(button);
 
-button.addEventListener('click', function(e) {
+button.addEventListener("click", function (e) {
     e.preventDefault();
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("password-confirm").value;
+    const errorPassword = document.getElementById("error-password");
     if (password !== confirmPassword) {
-        errorPassword.textContent = "La password non coincide"
+        errorPassword.textContent = "La password non coincide";
     }
 
-    formRegistered.submit()
-})
+    formRegistered.submit();
+});
