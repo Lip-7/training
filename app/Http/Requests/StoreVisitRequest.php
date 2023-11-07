6e5a62f6e453b41b6004de694da30e9bbc2bceaf
Lip-7 <zhighofficial@gmail.com>
2023-11-07 16:28:34 +0100

    added request of visit

--- app/Http/Requests/StoreVisitRequest.php
+++ app/Http/Requests/StoreVisitRequest.php
@@ -22,7 +22,9 @@ class StoreVisitRequest extends FormRequest
     public function rules(): array
     {
         return [
-           //
+            'apartment_id' => 'required|integer|min:1',
+            'ip' => 'required|string|min:1|max:50',
+            'date' => 'required|date',
         ];
     }
 }
